cb0a469a2a7e30ca4c32a8868aa4be2a2aa2cff4
rahmona-henry <rahmona.henry@icloud.com>
2017-05-09 23:24:18 +1200

    test bootsrap

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app =express()
+
+
+app.use(express.static('client'))
+
+
+app.get('/', function(req,res){
+  res.send('index.html')
+})
+
+app.listen(3000,function(){
+  console.log('Listening on port 3000!')
+})
